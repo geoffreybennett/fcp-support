cf7a63bdc5b059ca550bb3a60abc13c3ab503137
Geoffrey D. Bennett <g@b4.vu>
2025-04-02 06:49:28 +1030

    Fix firmware version comparison in client/main.c
    add_found_firmware() and found_firmware_cmp() were comparing pointers
    to firmware versions rather than firmware versions.

--- client/main.c
+++ client/main.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 Geoffrey D. Bennett <g@b4.vu>
+// SPDX-FileCopyrightText: 2024-2025 Geoffrey D. Bennett <g@b4.vu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #define _GNU_SOURCE
@@ -144,8 +144,10 @@ static void add_found_firmware(
     // already have this firmware under a different name?
     if (found_firmware->firmware->usb_vid == firmware->usb_vid &&
         found_firmware->firmware->usb_pid == firmware->usb_pid &&
-        found_firmware->firmware->firmware_version ==
-          firmware->firmware_version)
+        firmware_cmp(
+          found_firmware->firmware->firmware_version,
+          firmware->firmware_version
+        ) == 0)
       return;
   }
 
@@ -229,12 +231,7 @@ static int found_firmware_cmp(const void *p1, const void *p2) {
     return 1;
 
   // compare firmware version, newest first
-  if (f1->firmware_version < f2->firmware_version)
-    return 1;
-  if (f1->firmware_version > f2->firmware_version)
-    return -1;
-
-  return 0;
+  return -firmware_cmp(f1->firmware_version, f2->firmware_version);
 }
 
 static void enum_firmwares(void) {
